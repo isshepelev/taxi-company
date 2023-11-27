1de582c6c8e85fd698557f065bb54fb112c67bbe
Ilya <isshepelev2004@gmail.com>
2023-11-27 20:10:14 +0300

    JwtFilter

---
+++ src/main/java/ru/taxicompany/taxicompany/config/filter/JwtRequestFilter.java
@@ -0,0 +1,62 @@
+package ru.taxicompany.taxicompany.config.filter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.taxicompany.taxicompany.util.JwtUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private final JwtUtils jwtUtils;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwt = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")){
+            jwt = authHeader.substring(7);
+
+            try {
+                username = jwtUtils.getUsername(jwt);
+            }catch (ExpiredJwtException e) {
+                log.debug("время жизни токена вышло");
+            } catch (SignatureException e) {
+                log.debug("Подпись неверная");
+            } catch (Exception e) {
+                log.error("Ошибка при обработке токена");
+            }
+
+        }else {
+            log.debug("Authorization header is missing or does not start with 'Bearer'");
+        }
+
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() != null){
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username,null, jwtUtils.getRoles(jwt).stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+            SecurityContextHolder.getContext().setAuthentication(token);
+            log.info("User authenticated " + username);
+        }
+
+
+        filterChain.doFilter(request,response);
+    }
+}
