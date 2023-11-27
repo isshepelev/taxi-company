a6b8327c64ca91074ed391cc994793b2fc0accdf
Ilya <isshepelev2004@gmail.com>
2023-11-27 20:07:40 +0300

    first commit

---
+++ src/main/java/ru/taxicompany/taxicompany/TaxiCompanyApplication.java
@@ -0,0 +1,13 @@
+package ru.taxicompany.taxicompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/taxicompany/taxicompany/TaxiCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taxicompany.taxicompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
