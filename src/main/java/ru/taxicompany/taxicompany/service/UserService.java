c53891556a2cd62a9b149649588bb76d8dc3ab28
Ilya <isshepelev2004@gmail.com>
2023-11-28 01:15:47 +0300

    get rent car

--- src/main/java/ru/taxicompany/taxicompany/service/UserService.java
+++ src/main/java/ru/taxicompany/taxicompany/service/UserService.java
@@ -5,9 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import ru.taxicompany.taxicompany.domain.User;
+import ru.taxicompany.taxicompany.domain.UsersCars;
 import ru.taxicompany.taxicompany.dto.RegistrationUserDTO;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserService extends UserDetailsService {
@@ -15,4 +17,8 @@ public interface UserService extends UserDetailsService {
     Optional<User> findByUsername(String username);
 
     User createNewUser(RegistrationUserDTO registrationUserDto);
+
+    Object rentCar(String name, Long carId);
+
+    ResponseEntity<List<UsersCars>> getAllRentedCars();
 }
