61484c49f6a14d135998ed2aa49b545dda3519fb
Ilya <isshepelev2004@gmail.com>
2023-11-27 23:26:05 +0300

    add CarService

---
+++ src/main/java/ru/taxicompany/taxicompany/service/CarService.java
@@ -0,0 +1,18 @@
+package ru.taxicompany.taxicompany.service;
+
+import org.springframework.http.ResponseEntity;
+import ru.taxicompany.taxicompany.domain.Car;
+import ru.taxicompany.taxicompany.dto.CarDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CarService {
+    List<Car> getAllCars();
+
+    Car addNewCar(CarDTO carDTO);
+
+    Optional<Car> findByCarId(Long id);
+
+    void deleteCar(Long id);
+}
---
+++ src/main/java/ru/taxicompany/taxicompany/service/impl/CarServiceImpl.java
@@ -0,0 +1,44 @@
+package ru.taxicompany.taxicompany.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.taxicompany.taxicompany.domain.Car;
+import ru.taxicompany.taxicompany.dto.CarDTO;
+import ru.taxicompany.taxicompany.repository.CarRepository;
+import ru.taxicompany.taxicompany.service.CarService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CarServiceImpl implements CarService {
+    private final CarRepository carRepository;
+
+
+    @Override
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
+
+    @Override
+    public Car addNewCar(CarDTO carDTO) {
+        Car car = new Car();
+        car.setYear(carDTO.getYear());
+        car.setPrice(carDTO.getPrice());
+        car.setModel(carDTO.getModel());
+        car.setManufacturer(carDTO.getManufacturer());
+        car.setMileage(0);
+        return carRepository.save(car);
+    }
+
+    @Override
+    public Optional<Car> findByCarId(Long id){
+        return carRepository.findById(id);
+    }
+
+    @Override
+    public void deleteCar(Long id) {
+        carRepository.deleteById(id);
+    }
+}
