f0e2df2aeaa41d1724ee90882d61bd1c30a0d318
Ilya <isshepelev2004@gmail.com>
2023-11-27 20:08:14 +0300

    add JwtUtils

---
+++ src/main/java/ru/taxicompany/taxicompany/util/JwtUtils.java
@@ -0,0 +1,48 @@
+package ru.taxicompany.taxicompany.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+    private final String secret = "984hg493gh0439rthr0429uruj2309yh937gc763fe87t3f89723gf";
+    private Duration lifetime = Duration.ofMinutes(10);
+
+
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        List<String> roleList = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        claims.put("roles", roleList);
+
+        Date issuedDate = new Date();
+        Date expiredDate = new Date(issuedDate.getTime() + lifetime.toMillis());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(issuedDate)
+                .setExpiration(expiredDate)
+                .signWith(SignatureAlgorithm.HS256,secret).compact();
+    }
+
+    public String getUsername(String token){
+        return getAllClaimsFromToken(token).getSubject();
+    }
+    public List<String> getRoles(String token){
+        return getAllClaimsFromToken(token).get("roles", List.class);
+    }
+    public Claims getAllClaimsFromToken(String token){
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
