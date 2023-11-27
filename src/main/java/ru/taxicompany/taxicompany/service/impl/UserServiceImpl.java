df8c1b417e8a996d26c5ca161d212246d9f6722c
Ilya <isshepelev2004@gmail.com>
2023-11-28 01:15:49 +0300

    get rent car

--- src/main/java/ru/taxicompany/taxicompany/service/impl/UserServiceImpl.java
+++ src/main/java/ru/taxicompany/taxicompany/service/impl/UserServiceImpl.java
@@ -1,17 +1,24 @@
 package ru.taxicompany.taxicompany.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.taxicompany.taxicompany.domain.Car;
 import ru.taxicompany.taxicompany.domain.User;
+import ru.taxicompany.taxicompany.domain.UsersCars;
 import ru.taxicompany.taxicompany.dto.RegistrationUserDTO;
+import ru.taxicompany.taxicompany.exception.AppError;
 import ru.taxicompany.taxicompany.repository.UserRepository;
+import ru.taxicompany.taxicompany.service.CarService;
 import ru.taxicompany.taxicompany.service.RoleService;
 import ru.taxicompany.taxicompany.service.UserService;
+import ru.taxicompany.taxicompany.service.UsersCarsService;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -23,7 +30,17 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private UserRepository userRepository;
     private RoleService roleService;
     private PasswordEncoder passwordEncoder;
+    private CarService carService;
+    private UsersCarsService usersCarsService;
 
+    @Autowired
+    public void setUsersCarsService(UsersCarsService usersCarsService) {
+        this.usersCarsService = usersCarsService;
+    }
+    @Autowired
+    public void setCarService(CarService carService) {
+        this.carService = carService;
+    }
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -65,5 +82,39 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         user.setPassword(passwordEncoder.encode(registrationUserDto.getPassword()));
         user.setRoles(List.of(roleService.getUserRole()));
         return userRepository.save(user);
+    }
+
+    @Override
+    public Object rentCar(String name, Long carId) {
+        Optional<User> userOptional = findByUsername(name);
+        Optional<Car> carOptional = carService.findByCarId(carId);
+        if (carOptional.isEmpty()){
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "машина с таким id не найдена"), HttpStatus.BAD_REQUEST);
+        }
+
+        Car car = carOptional.get();
+        User user = userOptional.get();
+
+        if (car.isAvailable()){
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "машина с таким id уже занята"), HttpStatus.BAD_REQUEST);
+        }
+
+        user.getCars().add(car);
+        userRepository.save(user);
+
+        UsersCars usersCars = new UsersCars();
+        usersCars.setCar(car);
+        usersCars.setUser(user);
+        usersCarsService.addUsersCars(usersCars);
+
+        car.setAvailable(true);
+        carService.save(car);
+
+        return ResponseEntity.ok("success");
+    }
+
+    @Override
+    public ResponseEntity<List<UsersCars>> getAllRentedCars() {
+        return ResponseEntity.ok(usersCarsService.getAllUsersCars());
     }
 }
