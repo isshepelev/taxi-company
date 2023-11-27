94233c88d431af2284cc91c10fd43d543f5fbe3d
Ilya <isshepelev2004@gmail.com>
2023-11-28 01:16:43 +0300

    add rent car controller

--- src/main/java/ru/taxicompany/taxicompany/controllers/UserController.java
+++ src/main/java/ru/taxicompany/taxicompany/controllers/UserController.java
@@ -1,9 +1,27 @@
 package ru.taxicompany.taxicompany.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.taxicompany.taxicompany.domain.UsersCars;
+import ru.taxicompany.taxicompany.service.UserService;
+
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/rent-car/{carId}")
+    public ResponseEntity<?> userRentCar(Principal principal, @PathVariable Long carId){
+        return ResponseEntity.ok(userService.rentCar(principal.getName(), carId));
+    }
+    @GetMapping("/rented-cars")
+    public ResponseEntity<List<UsersCars>> getRentedCars(){
+        return userService.getAllRentedCars();
+    }
 }
