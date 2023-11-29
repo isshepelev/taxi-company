70de8c09d13f56a3170a87e258d13022d3918d3f
Ilya <isshepelev2004@gmail.com>
2023-11-29 03:02:29 +0300

    add annotation

--- src/main/java/ru/taxicompany/taxicompany/repository/RoleRepository.java
+++ src/main/java/ru/taxicompany/taxicompany/repository/RoleRepository.java
@@ -1,10 +1,12 @@
 package ru.taxicompany.taxicompany.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.taxicompany.taxicompany.domain.Role;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
