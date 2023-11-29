fa0d7aaab55b814cf4070374068017eeed744b2a
Ilya <isshepelev2004@gmail.com>
2023-11-30 01:12:28 +0300

    add method delete user and give role

--- src/main/java/ru/taxicompany/taxicompany/service/impl/AdminServiceImpl.java
+++ src/main/java/ru/taxicompany/taxicompany/service/impl/AdminServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.taxicompany.taxicompany.domain.Car;
+import ru.taxicompany.taxicompany.domain.Role;
 import ru.taxicompany.taxicompany.domain.User;
 import ru.taxicompany.taxicompany.dto.CarDTO;
 import ru.taxicompany.taxicompany.exception.AppError;
@@ -13,8 +14,10 @@ import ru.taxicompany.taxicompany.service.CarService;
 import ru.taxicompany.taxicompany.service.UserService;
 import ru.taxicompany.taxicompany.service.UsersCarsService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -49,5 +52,40 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public ResponseEntity<List<User>> getAllUser() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @Override
+    public ResponseEntity<?> deleteUser(String username) {
+        Optional<User> userOptional = userService.findByUsername(username);
+        if (userOptional.isEmpty()){
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "пользователя с именем " +username+ " не существует"), HttpStatus.BAD_REQUEST);
+        }
+        User user = userOptional.get();
+        if (!user.getCars().isEmpty()){
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "пользователь не может быть удален так как он арендует машину"), HttpStatus.BAD_REQUEST);
+        }
+        userService.deleteUserByUsername(username);
+        return ResponseEntity.ok("success");
+    }
+
+    @Override
+    public ResponseEntity<?> giveAdmin(String username) {
+        Optional<User> userOptional = userService.findByUsername(username);
+        if (userOptional.isEmpty()){
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "пользователя не существует"), HttpStatus.BAD_REQUEST);
+        }
+
+        User user = userOptional.get();
+        if (user.getRoles().stream().map(Role::getName).anyMatch(name -> name.equals("ROLE_ADMIN"))){
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "пользователь уже администратор"), HttpStatus.BAD_REQUEST);
+        }
+        Role role = new Role();
+        role.setId(2L);
+        role.setName("ROLE_ADMIN");
+        List<Role> roleAdminList = new ArrayList<>();
+        roleAdminList.add(role);
+        user.setRoles(roleAdminList);
+        userService.save(user);
+        return ResponseEntity.ok("success");
     }
 }
