35d28bfe4280bbd5eb43b997f6c3574357b95d2d
Ilya <isshepelev2004@gmail.com>
2023-11-29 03:03:04 +0300

    add findByCar

--- src/main/java/ru/taxicompany/taxicompany/repository/UsersCarsRepository.java
+++ src/main/java/ru/taxicompany/taxicompany/repository/UsersCarsRepository.java
@@ -2,8 +2,12 @@ package ru.taxicompany.taxicompany.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.taxicompany.taxicompany.domain.Car;
 import ru.taxicompany.taxicompany.domain.UsersCars;
+
+import java.util.Optional;
 
 @Repository
 public interface UsersCarsRepository extends JpaRepository<UsersCars, Long> {
+    Optional<UsersCars> findByCar(Car car);
 }
