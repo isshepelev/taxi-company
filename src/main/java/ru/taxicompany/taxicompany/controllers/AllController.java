e5c7f4aae07fcb170d7d6eb0abe99f0c2a58f5a7
Ilya <isshepelev2004@gmail.com>
2023-11-29 03:02:00 +0300

    fixed

--- src/main/java/ru/taxicompany/taxicompany/controllers/AllController.java
+++ src/main/java/ru/taxicompany/taxicompany/controllers/AllController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.taxicompany.taxicompany.domain.Car;
+import ru.taxicompany.taxicompany.domain.UsersCars;
 import ru.taxicompany.taxicompany.service.AllService;
+import ru.taxicompany.taxicompany.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/all")
 @RequiredArgsConstructor
 public class AllController {
     private final AllService allService;
+    private final UserService userService;
 
     @GetMapping("/cars")
     public ResponseEntity<?> getAllCars(){
@@ -23,5 +28,9 @@ public class AllController {
     @GetMapping("/car/{id}")
     public ResponseEntity<?> getCat(@PathVariable Long id){
         return allService.getCar(id);
+    }
+    @GetMapping("/rented-cars")
+    public ResponseEntity<List<UsersCars>> getRentedCars(){
+        return userService.getAllRentedCars();
     }
 }
