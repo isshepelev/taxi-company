26eb2bede334243d75eaec724d1fa92d3901b42e
Ilya <isshepelev2004@gmail.com>
2023-11-29 03:01:00 +0300

    browsing users endpoint

--- src/main/java/ru/taxicompany/taxicompany/service/AdminService.java
+++ src/main/java/ru/taxicompany/taxicompany/service/AdminService.java
@@ -1,10 +1,15 @@
 package ru.taxicompany.taxicompany.service;
 
 import org.springframework.http.ResponseEntity;
+import ru.taxicompany.taxicompany.domain.User;
 import ru.taxicompany.taxicompany.dto.CarDTO;
+
+import java.util.List;
 
 public interface AdminService {
     ResponseEntity<?> addCar(CarDTO carDTO);
 
     ResponseEntity<?> deleteCar(Long id);
+
+    ResponseEntity<List<User>> getAllUser();
 }
