54c69b8361b929d6ff111f2806f76781e837786c
Ilya <isshepelev2004@gmail.com>
2023-12-04 22:22:31 +0300

    add flyway

---
+++ src/main/resources/db/migration/V5__insert_user.sql
@@ -0,0 +1,6 @@
+insert into users (username, password) VALUES ('admin', '$2a$10$DWPvaWiyOkj1p4Fh8yttWOwLssK8ax7k69Swe1UD1rZ5/w9J9m87W');
+insert into users (username, password) VALUES ('user1', '$2a$10$ee18PJTwkq7J7GhxzCoVhefndBLJCvldBrYIqtW2R4lYYhv4vjjbC');
+insert into users (username, password) VALUES ('user2', '$2a$10$ee18PJTwkq7J7GhxzCoVhefndBLJCvldBrYIqtW2R4lYYhv4vjjbC');
+insert into users (username, password) VALUES ('user3', '$2a$10$ee18PJTwkq7J7GhxzCoVhefndBLJCvldBrYIqtW2R4lYYhv4vjjbC');
+insert into users (username, password) VALUES ('user4', '$2a$10$ee18PJTwkq7J7GhxzCoVhefndBLJCvldBrYIqtW2R4lYYhv4vjjbC');
+insert into users (username, password) VALUES ('user5', '$2a$10$ee18PJTwkq7J7GhxzCoVhefndBLJCvldBrYIqtW2R4lYYhv4vjjbC');
---
+++ src/main/resources/db/migration/V6__insert_user_roles.sql
@@ -0,0 +1,6 @@
+insert into users_roles (user_username, roles_id) VALUES ('admin', 2);
+insert into users_roles (user_username, roles_id) VALUES ('user1', 1);
+insert into users_roles (user_username, roles_id) VALUES ('user2', 1);
+insert into users_roles (user_username, roles_id) VALUES ('user3', 1);
+insert into users_roles (user_username, roles_id) VALUES ('user4', 1);
+insert into users_roles (user_username, roles_id) VALUES ('user5', 1);
