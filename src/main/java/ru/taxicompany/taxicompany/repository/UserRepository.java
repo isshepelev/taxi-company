9368f843978b575210f22cf507a9269d611f6cde
Ilya <isshepelev2004@gmail.com>
2023-12-05 02:06:09 +0300

    reformat code

--- src/main/java/ru/taxicompany/taxicompany/repository/UserRepository.java
+++ src/main/java/ru/taxicompany/taxicompany/repository/UserRepository.java
@@ -7,6 +7,6 @@ import ru.taxicompany.taxicompany.domain.User;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
 }
