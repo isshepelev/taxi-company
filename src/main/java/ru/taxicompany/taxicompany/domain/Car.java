4d5e46ce3f314625d9a3cfc4b6dc8efffef50a86
Ilya <isshepelev2004@gmail.com>
2023-12-09 18:42:44 +0300

    add annotation

--- src/main/java/ru/taxicompany/taxicompany/domain/Car.java
+++ src/main/java/ru/taxicompany/taxicompany/domain/Car.java
@@ -1,7 +1,9 @@
 package ru.taxicompany.taxicompany.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
